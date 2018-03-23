39fd2a59861ab21ee5f01f9e7672c850aea5100e
Ron Southwick <rsouthwick@fool.com>
2018-03-23 15:14:50 -0400

    you gotta gotta setup to set down

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="spambot",
+    url="https://github.com/rsouthwi/Spambot",
+    description="a bot whose primary function is to spam",
+    author="Ron Southwick",
+    author_email="github@fool.com",
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    platforms='any',
+    install_requires=[
+    ],
+    use_scm_version=True,
+    skip_upload_docs=True,
+    setup_requires=[
+
+    ],
+    packages=find_packages(exclude=['tests']),
+    test_suite="tests"
+)
