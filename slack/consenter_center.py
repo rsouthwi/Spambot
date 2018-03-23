581eafb64da5631b7ffac778717cfa35448ff91b
Ron Southwick <rsouthwick@fool.com>
2018-03-23 15:17:27 -0400

    a class to spam yourself

--- slack/consenter_center.py
+++ slack/consenter_center.py
@@ -1,8 +1,16 @@
 
 
+class PrintHook(object):
+    def __init__(self, *args, **kwargs):
+        print("args: {}".format(args))
+        print("kwargs: {}".format(kwargs))
+        
+
 class NewLegaleseHook(object):
     pass
 
 
 class CustomerConsentHook(object):
     pass
+
+
